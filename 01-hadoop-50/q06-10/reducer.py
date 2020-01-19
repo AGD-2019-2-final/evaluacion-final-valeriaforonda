92d0428d77f7200ab98dea7763deef670273cb1e
Valeria Foronda Carmona <50217454+valeriaforonda@users.noreply.github.com>
2020-01-19 13:46:04 -0500

    Avance Hadoop

--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -1,4 +1,48 @@
 import sys
 #
-# >>> Escriba el codigo del mapper a partir de este punto <<<
+#  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == '__main__':
+
+    curkey = None
+    totalmax = 0
+    totalmin = 2000
+
+    ##
+    ## cada linea de texto recibida es una
+    ## entrada clave \tabulador valor
+    ##
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+
+        if key == curkey:
+            ##
+            ## No se ha cambiado de clave. Aca se
+            ## acumulan los valores para la misma clave.
+            ##
+            if val > totalmax:
+                totalmax = val
+
+            if val < totalmin:
+                totalmin = val
+            
+        else:
+            ##
+            ## Se cambio de clave. Se reinicia el
+            ## acumulador.
+            ##
+            if curkey is not None:
+                ##
+                ## una vez se han reducido todos los elementos
+                ## con la misma clave se imprime el resultado en
+                ## el flujo de salida
+                ##
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, totalmax, totalmin))
+
+            curkey = key
+            totalmax = val
+            totalmin = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, totalmax, totalmin))
+
